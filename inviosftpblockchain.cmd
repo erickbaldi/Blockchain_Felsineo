fa7270d9bff1dd2d279032140671d5aedda551e4
erickbaldi <erickbaldi@yahoo.it>
2025-07-17 12:48:20 +0200

    first pubblication, commit and push

---
+++ inviosftpblockchain.cmd
@@ -0,0 +1,8 @@
+cd\
+winscp.com /log="C:\Batch\Blockchain\WinSCPBlockchain.log" /ini=nul /script="C:\Batch\Blockchain\scriptblockchain.txt"
+cd C:\batch\blockchain\
+
+del blockchain_fmv.xls
+del blockchain_fels.xls
+quit
+exit
